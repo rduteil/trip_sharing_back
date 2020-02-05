6b536e2a0a24dc3c63e4cc3807398fa0cfdfec3f
rduteil <53625621+rduteil@users.noreply.github.com>
2020-02-05 15:08:37 +0100

    Various fixes

---
+++ test/test.js
@@ -0,0 +1,10 @@
+const supertest = require("supertest");
+const app = require("../src/index.js");
+
+describe("GET /", function() {
+  it("Cannot GET /", function(done) {
+    supertest(app)
+      .get("/")
+      .expect("Cannot GET /", done);
+  });
+});
